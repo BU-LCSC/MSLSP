5a75f49b93374ed359f1dded27d01059d5ffae0e
minkyum <moon.minkyu@gmail.com>
2020-12-21 14:24:12 -0500

    delete unnecessary lines

--- Development/run_sh_script.R
+++ Development/run_sh_script.R
@@ -9,8 +9,3 @@ for(tt in 1:10){
 }
 
 
-# Check AWS results
-setwd('/projectnb/modislc/users/mkmoon/MuSLI/V1_0/figures/')
-for(tt in 1:3){
-  system(paste('qsub -V -pe omp 2 -l h_rt=06:00:00 /usr3/graduate/mkmoon/GitHub/MSLSP/Development/run_script.sh ',tt,sep=''))  
-}
