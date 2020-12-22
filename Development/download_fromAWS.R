4d16c133a2968e7dd16d646e24b44df5dd31922d
minkyum <moon.minkyu@gmail.com>
2020-12-22 09:18:44 -0500

    more tiles

--- Development/download_fromAWS.R
+++ Development/download_fromAWS.R
@@ -3,6 +3,8 @@
 
 # tiles <- c('13TEF','16TCK','17SQD','18TYN','19TEL')
 tiles <- c('18TXS','18TXT','18TYL','18TYM','18TYN','18TYP','18TYQ','18TYR','18TYS','18TYT') # 12/20/2020
+tiles <- c('13SBA','13SBB','13SBC','13SBD','13SBR','13SBS','13SBT','13SBU','13SBV','13SCA', # 12/21/2020
+           '15TTE','15TTF','15TTG','15TUE','15TUF') 
 
 # Recursive - phenometrics file only
 for(i in 1:length(tiles)){
@@ -11,10 +13,10 @@ for(i in 1:length(tiles)){
   }
 }
 
-# # Recursive - all files 
-# for(i in 1:length(tiles)){
-#   system(paste('aws s3 cp s3://mslsp/v1.0/product/',tiles[i],' /projectnb/modislc/users/mkmoon/MuSLI/V1_0/From_AWS/product/',tiles[i],'/ --recursive',sep=''))  
-# }
+# Recursive - all files
+for(i in 1:length(tiles)){
+  system(paste('aws s3 cp s3://mslsp/v1.0/product/',tiles[i],' /projectnb/modislc/users/mkmoon/MuSLI/V1_0/From_AWS/product/',tiles[i],'/ --recursive',sep=''))
+}
 
 # # V0 log info
 # system('aws s3 cp s3://mslsp/logs/instanceInfo/ /projectnb/modislc/users/mkmoon/MuSLI/V1_0/product_qc/v0_loginfo/ --recursive' )
