15cfb633760e0923fee6a09a6130667dc660e901
minkyum <moon.minkyu@gmail.com>
2023-02-07 22:41:14 -0500

    minor

--- SCC/MSLSP_submitTiles_SCC.sh
+++ SCC/MSLSP_submitTiles_SCC.sh
@@ -66,7 +66,7 @@ else
         tileDir="${workDir}${tile}/"
         paramName="${tileDir}parameters_${jobTime}.json"
         nameArg="-N R_${tile}"
-        logArg="-o ${workDir}Run_${tile}_${jobTime}.txt"
+        logArg="-o ${logDir}Run_${tile}_${jobTime}.txt"
         qsub $nameArg $logArg $nodeArgs MSLSP_runTile_SCC.sh $tile $paramName $jobTime
     done < $tileList
 fi 
