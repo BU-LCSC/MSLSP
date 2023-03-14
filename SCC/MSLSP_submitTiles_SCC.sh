439325c7d8114e3ac70bee25385544ffb8facc71
minkyum <moon.minkyu@gmail.com>
2023-03-14 11:50:15 -0400

    minor

--- SCC/MSLSP_submitTiles_SCC.sh
+++ SCC/MSLSP_submitTiles_SCC.sh
@@ -2,7 +2,7 @@
 module load jq
 
 parameters="/usr3/graduate/mkmoon/GitHub/MSLSP/MSLSP_Parameters.json"
-tileList="tileLists/cr_etc.txt"
+tileList="tileLists/cr_LA.txt"
 
 numCores=$( jq .SCC.numCores $parameters )
 workDir=$( jq --raw-output .SCC.workDir $parameters )
