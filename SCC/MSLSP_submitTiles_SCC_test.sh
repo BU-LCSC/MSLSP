745422c9a1c63e51089bc792ed6d2a0cdc873806
seamore-z <zhu.seamore@gmail.com>
2025-01-14 17:58:06 -0500

    Adding testing scripts for code improvement

---
+++ SCC/MSLSP_submitTiles_SCC_test.sh
@@ -0,0 +1,73 @@
+
+module load jq
+
+parameters="/usr3/graduate/seamorez/GitHub/MSLSP/MSLSP_Parameters_test.json"
+tileList="tileLists/test_tile.txt"
+
+numCores=$( jq .SCC.numCores $parameters )
+workDir=$( jq --raw-output .SCC.workDir $parameters )
+dataDir=$( jq --raw-output .SCC.dataDir $parameters )
+
+jobTime="$(date +%Y_%m_%d_%H_%M_%S)"
+
+logDir=$( jq --raw-output .SCC.logDir $parameters ) 
+mkdir -p $logDir
+
+if $(jq .SCC.runS10 $parameters )
+then
+    baseDir="${dataDir}S10/"
+    nodeArgs="-l h_rt=24:00:00 -l mem_per_core=16G -pe mpi_16_tasks_per_node ${numCores}"
+else
+    baseDir="${dataDir}HLS30/"
+    nodeArgs="-l h_rt=36:00:00 -pe omp ${numCores} -l mem_per_core=13G" # SLOWER SCHEDULING, FASTER EXEC
+    # nodeArgs="-l h_rt=36:00:00 -pe omp 28 -l mem_per_core=9G" # FASTER SCHEDULING, SLOWER EXEC (MAKE SURE TO CHANGE PARAMS JSON > NUMCORES TO 14)
+fi
+
+
+#Set up initial folders and copy parameter file 
+while read -r tile
+    do
+        tileDir="${workDir}${tile}/"
+        imgDir="${baseDir}${tile}/images/"
+        mkdir -p $tileDir
+        mkdir -p $imgDir
+        
+        paramName="${tileDir}parameters_${jobTime}.json"
+        jq --arg imgDir "$imgDir" '.dirs.imgDir = $imgDir' ${parameters}>${paramName}
+        
+    done < $tileList
+
+
+#If download is set to true, the code will check for new HLS images and only download those
+if [ $(jq .setup.downloadImagery $parameters ) == true ] && [ $(jq .SCC.runS10 $parameters ) == false ]
+then
+    while read -r tile
+    do
+        nameArg="-N DL_${tile}"
+        logArg_download="-o ${logDir}Download_${tile}_${jobTime}.txt"
+        downloadArg="-l download"
+        imgStartYr=$(( $( jq .setup.imgStartYr $parameters )))  
+        imgEndYr=$(( $( jq .setup.imgEndYr $parameters )))      
+        qsub $nameArg $logArg_download $downloadArg SCC/runDownloadHLS.sh $tile $baseDir $imgStartYr $imgEndYr
+    done < $tileList
+
+    while read -r tile
+    do
+        tileDir="${workDir}${tile}/"
+        paramName="${tileDir}parameters_${jobTime}.json"
+        nameArg="-N R_${tile}"
+        logArg="-o ${logDir}Run_${tile}_${jobTime}.txt"
+        holdArg="-hold_jid DL_${tile}"
+        qsub $nameArg $logArg $nodeArgs $holdArg SCC/MSLSP_runTile_SCC.sh $tile $paramName $jobTime
+    done < $tileList
+else
+    while read -r tile
+    do
+        tileDir="${workDir}${tile}/"
+        paramName="${tileDir}parameters_${jobTime}.json"
+        nameArg="-N R_${tile}"
+        logArg="-o ${logDir}Run_${tile}_${jobTime}.txt"
+        qsub $nameArg $logArg $nodeArgs SCC/MSLSP_runTile_SCC.sh $tile $paramName $jobTime
+    done < $tileList
+fi
+
